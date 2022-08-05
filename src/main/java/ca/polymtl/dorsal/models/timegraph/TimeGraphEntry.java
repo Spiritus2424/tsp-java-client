afcf9ea574a0adad2552eb2ea3650df9dacd78b6
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:52:24 +0000

    add constructor and getter setter for timegraph package

--- src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphEntry.java
+++ src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphEntry.java
@@ -5,4 +5,25 @@ import java.math.BigInteger;
 public class TimeGraphEntry {
     private BigInteger start;
     private BigInteger end;
+
+    public TimeGraphEntry(BigInteger start, BigInteger end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public BigInteger getStart() {
+        return start;
+    }
+
+    public void setStart(BigInteger start) {
+        this.start = start;
+    }
+
+    public BigInteger getEnd() {
+        return end;
+    }
+
+    public void setEnd(BigInteger end) {
+        this.end = end;
+    }
 }
