a7090f9191ce9cc6bd446ae9b516bde1271c440b
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:46:45 +0000

    add constructor and getter setter for table package

--- src/main/java/ca/polymtl/dorsal/models/table/Cell.java
+++ src/main/java/ca/polymtl/dorsal/models/table/Cell.java
@@ -3,4 +3,29 @@ package ca.polymtl.dorsal.models.table;
 public class Cell {
     private String content;
     private int tags;
+
+    public Cell(String content) {
+        this.content = content;
+    }
+
+    public Cell(String content, int tags) {
+        this.content = content;
+        this.tags = tags;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getTags() {
+        return tags;
+    }
+
+    public void setTags(int tags) {
+        this.tags = tags;
+    }
 }
