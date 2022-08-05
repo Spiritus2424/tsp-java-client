7f6f3be68c741972d372bbf97cdbd9221b8a9060
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:30:42 +0000

    add constructor and getter setter for entry package

--- src/main/java/ca/polymtl/dorsal/models/entry/EntryHeader.java
+++ src/main/java/ca/polymtl/dorsal/models/entry/EntryHeader.java
@@ -3,4 +3,25 @@ package ca.polymtl.dorsal.models.entry;
 public class EntryHeader {
     private String name;
     private String tooltip;
+
+    public EntryHeader(String name, String tooltip) {
+        this.name = name;
+        this.tooltip = tooltip;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTooltip() {
+        return tooltip;
+    }
+
+    public void setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+    }
 }
