d04963955513082282977c24ff4548a692aefdda
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:52:24 +0000

    add constructor and getter setter for timegraph package

--- src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphArrow.java
+++ src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphArrow.java
@@ -10,4 +10,59 @@ public class TimeGraphArrow {
     private BigInteger start;
     private BigInteger end;
     private OutputElementStyle style;
+
+    public TimeGraphArrow(int sourceId, int targetId, BigInteger start, BigInteger end) {
+        this.sourceId = sourceId;
+        this.targetId = targetId;
+        this.start = start;
+        this.end = end;
+    }
+
+    public TimeGraphArrow(int sourceId, int targetId, BigInteger start, BigInteger end, OutputElementStyle style) {
+        this.sourceId = sourceId;
+        this.targetId = targetId;
+        this.start = start;
+        this.end = end;
+        this.style = style;
+    }
+
+    public int getSourceId() {
+        return sourceId;
+    }
+
+    public void setSourceId(int sourceId) {
+        this.sourceId = sourceId;
+    }
+
+    public int getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(int targetId) {
+        this.targetId = targetId;
+    }
+
+    public BigInteger getStart() {
+        return start;
+    }
+
+    public void setStart(BigInteger start) {
+        this.start = start;
+    }
+
+    public BigInteger getEnd() {
+        return end;
+    }
+
+    public void setEnd(BigInteger end) {
+        this.end = end;
+    }
+
+    public OutputElementStyle getStyle() {
+        return style;
+    }
+
+    public void setStyle(OutputElementStyle style) {
+        this.style = style;
+    }
 }
--- src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphModel.java
+++ src/main/java/ca/polymtl/dorsal/models/timegraph/TimeGraphModel.java
@@ -2,4 +2,16 @@ package ca.polymtl.dorsal.models.timegraph;
 
 public class TimeGraphModel {
     private TimeGraphRow rows;
+
+    public TimeGraphModel(TimeGraphRow rows) {
+        this.rows = rows;
+    }
+
+    public TimeGraphRow getRows() {
+        return rows;
+    }
+
+    public void setRows(TimeGraphRow rows) {
+        this.rows = rows;
+    }
 }
