9300eaed64714079d9951907d30d7694113affc6
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:44:30 +0000

    add filter class

---
+++ src/main/java/ca/polymtl/dorsal/models/filter/Filter.java
@@ -0,0 +1,13 @@
+package ca.polymtl.dorsal.models.filter;
+
+import java.math.BigInteger;
+
+public class Filter {
+    private String id;
+    private String name;
+    private BigInteger startTime;
+    private BigInteger endTime;
+    private String expression;
+    private int tags;
+
+}
