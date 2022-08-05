88ef330de02b57ee70acc0677e85b4a7fc7f1429
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:43:01 +0000

    add bookmark class

---
+++ src/main/java/ca/polymtl/dorsal/models/bookmark/Bookmark.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+
+public class Bookmark {
+    private String UUID;
+    private String name;
+    private BigInteger startTime;
+    private BigInteger endTime;
+    private String type;
+}
