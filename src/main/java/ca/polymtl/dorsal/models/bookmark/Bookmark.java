41fa3b3868cf7bfa5022042e36d51cefd6e779a7
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:20:27 +0000

    generate constructor and getter setter for bookmark package

--- src/main/java/ca/polymtl/dorsal/models/bookmark/Bookmark.java
+++ src/main/java/ca/polymtl/dorsal/models/bookmark/Bookmark.java
@@ -6,4 +6,53 @@ public class Bookmark {
     private BigInteger startTime;
     private BigInteger endTime;
     private String type;
+
+    public Bookmark(String uUID, String name, BigInteger startTime, BigInteger endTime, String type) {
+        UUID = uUID;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.type = type;
+    }
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String uUID) {
+        UUID = uUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigInteger getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(BigInteger startTime) {
+        this.startTime = startTime;
+    }
+
+    public BigInteger getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(BigInteger endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
 }
