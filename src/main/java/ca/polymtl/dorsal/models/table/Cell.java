1cb345b604c2635bdd83f20cb73c55dab48aec6f
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:47:10 +0000

    add table module class

---
+++ src/main/java/ca/polymtl/dorsal/models/table/Cell.java
@@ -0,0 +1,6 @@
+package ca.polymtl.dorsal.models.table;
+
+public class Cell {
+    private String content;
+    private int tags;
+}
---
+++ src/main/java/ca/polymtl/dorsal/models/table/TableModel.java
@@ -0,0 +1,10 @@
+package ca.polymtl.dorsal.models.table;
+
+import java.util.ArrayList;
+
+public class TableModel {
+    private int lowIndex;
+    private int size;
+    private ArrayList<Integer> columnIds;
+    private ArrayList<Line> lines;
+}
