ea3ff215702d781bb1ba5abe6e7e35a3bb48b643
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:47:10 +0000

    add table module class

---
+++ src/main/java/ca/polymtl/dorsal/models/table/ColumnHeaderEntry.java
@@ -0,0 +1,8 @@
+package ca.polymtl.dorsal.models.table;
+
+public class ColumnHeaderEntry {
+    private int id;
+    private String name;
+    private String description;
+    private String type;
+}
