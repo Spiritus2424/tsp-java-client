a8acfe06f1d055a3cac3d1eb331e0432bbac6b24
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:42:27 +0000

    add annotation class

---
+++ src/main/java/ca/polymtl/dorsal/models/annotation/AnnotationCategoriesModel.java
@@ -0,0 +1,11 @@
+package ca.polymtl.dorsal.models.annotation;
+
+import java.util.ArrayList;
+
+/**
+ * AnnotationCategoriesModel
+ */
+public class AnnotationCategoriesModel {
+    private ArrayList<String> annotationCategories;
+
+}
