f51be00fa3d6d9476535d1b7f522da2dffe13c12
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 01:48:12 +0000

    add trace package class

---
+++ src/main/java/ca/polymtl/dorsal/models/trace/Trace.java
@@ -0,0 +1,13 @@
+package ca.polymtl.dorsal.models.trace;
+
+import java.math.BigInteger;
+
+public class Trace {
+    private String UUID;
+    private String name;
+    private BigInteger start;
+    private BigInteger end;
+    private String path;
+    private int nbEvents;
+    private String indexingStatus;
+}
