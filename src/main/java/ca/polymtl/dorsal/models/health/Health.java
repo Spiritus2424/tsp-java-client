3b413a7f1abcfac456378b21e62089b02ebe7933
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-05 18:36:28 +0000

    add constructor and getter setter for Health package

--- src/main/java/ca/polymtl/dorsal/models/health/Health.java
+++ src/main/java/ca/polymtl/dorsal/models/health/Health.java
@@ -2,4 +2,16 @@ package ca.polymtl.dorsal.models.health;
 
 public class Health {
     private HealthStatus status;
+
+    public Health(HealthStatus status) {
+        this.status = status;
+    }
+
+    public HealthStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HealthStatus status) {
+        this.status = status;
+    }
 }
