600554abc6739b625083737e133a795d5000f82d
Spiritus2424 <46603536+Spiritus2424@users.noreply.github.com>
2022-08-11 18:31:23 -0400

    Add Query package in Models package (#4)

---
+++ src/main/java/ca/polymtl/dorsal/models/query/QueryInterval.java
@@ -0,0 +1,45 @@
+package ca.polymtl.dorsal.models.query;
+
+import java.math.BigInteger;
+
+public class QueryInterval {
+    private BigInteger start;
+    private BigInteger end;
+    private int nbTimes;
+
+    public QueryInterval(BigInteger start, BigInteger end, int nbTimes) {
+        this.start = start;
+        this.end = end;
+        this.nbTimes = nbTimes;
+    }
+
+    public QueryInterval(BigInteger start, BigInteger end) {
+        this.start = start;
+        this.end = end;
+        this.nbTimes = 0;
+    }
+
+    public BigInteger getStart() {
+        return start;
+    }
+
+    public void setStart(BigInteger start) {
+        this.start = start;
+    }
+
+    public BigInteger getEnd() {
+        return end;
+    }
+
+    public void setEnd(BigInteger end) {
+        this.end = end;
+    }
+
+    public int getNbTimes() {
+        return nbTimes;
+    }
+
+    public void setNbTimes(int nbTimes) {
+        this.nbTimes = nbTimes;
+    }
+}
