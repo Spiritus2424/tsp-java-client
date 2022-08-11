36c4e9c7a417ae45c81ef2411793ae884b810787
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-08-11 01:40:53 +0000

    Implement QueryHelper static methods

--- src/main/java/ca/polymtl/dorsal/models/query/QueryHelper.java
+++ src/main/java/ca/polymtl/dorsal/models/query/QueryHelper.java
@@ -43,32 +43,83 @@ public class QueryHelper {
 
     public static Query timeRangeQuery(BigInteger start, BigInteger end,
             Optional<Map<String, Object>> additionalProperties) {
-        throw new UnsupportedOperationException();
+        return timeRangeQuery(start, end, Optional.empty(), additionalProperties);
     }
 
     public static Query timeRangeQuery(BigInteger start, BigInteger end,
             Optional<Integer> nbTimes,
             Optional<Map<String, Object>> additionalProperties) {
-        throw new UnsupportedOperationException();
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(REQUESTED_TIMERANGE_KEY, nbTimes.isPresent() ? new QueryInterval(start, end, nbTimes.get())
+                : new QueryInterval(start, end));
+        if (additionalProperties.isPresent()) {
+            parameters.putAll(additionalProperties.get());
+        }
+
+        return new Query(parameters);
     }
 
     public static Query selectionTimeRangeQuery(BigInteger start, BigInteger end, int nbTimes, int[] items,
             Optional<Map<String, Object>> additionalProperties) {
-        throw new UnsupportedOperationException();
+        return selectionTimeRangeQuery(start, end, Optional.of(nbTimes), Optional.of(items), additionalProperties);
     }
 
     public static Query selectionTimeRangeQuery(BigInteger start, BigInteger end, Optional<Integer> nbTimes,
             Optional<int[]> items,
             Optional<Map<String, Object>> additionalProperties) {
-        throw new UnsupportedOperationException();
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(REQUESTED_TIMERANGE_KEY, nbTimes.isPresent() ? new QueryInterval(start, end, nbTimes.get())
+                : new QueryInterval(start, end));
+
+        if (items.isPresent()) {
+            parameters.put(REQUESTED_ITEMS_KEY, items.get());
+        }
+
+        if (additionalProperties.isPresent()) {
+            parameters.putAll(additionalProperties.get());
+        }
+
+        return new Query(parameters);
     }
 
     public static Query tableQuery(int[] columnsId, int index, int count,
             Optional<Map<String, Object>> additionalProperties) {
-        throw new UnsupportedOperationException();
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(REQUESTED_TABLE_COLUMN_IDS_KEY, columnsId);
+        parameters.put(REQUESTED_TABLE_INDEX_KEY, index);
+        parameters.put(REQUESTED_TABLE_COUNT_KEY, count);
+        if (additionalProperties.isPresent()) {
+            parameters.putAll(additionalProperties.get());
+        }
+        return new Query(parameters);
     }
 
-    public static BigInteger[] splitRangeIntoEqualParts(BigInteger start, BigInteger end, int number) {
-        throw new UnsupportedOperationException();
+    public static BigInteger[] splitRangeIntoEqualParts(BigInteger start, BigInteger end, int size) {
+
+        if (start.compareTo(end) == 1) {
+            final BigInteger temp = end;
+            end = start;
+            start = temp;
+        }
+
+        if (size == 0) {
+            final BigInteger[] temp = {};
+            return temp;
+        } else if (size == 1) {
+            final BigInteger[] temp = { start };
+            return temp;
+        }
+
+        size = Math.min(size, end.subtract(start).add(BigInteger.ONE).intValue());
+
+        final BigInteger[] result = new BigInteger[size];
+        final int stepSize = Math.max(1, end.subtract(start).divide(BigInteger.valueOf(size - 1)).intValue());
+
+        for (int i = 0; i < size; i++) {
+            result[i] = start.add(BigInteger.valueOf(i * stepSize));
+        }
+
+        result[result.length - 1] = end;
+        return result;
     }
 }
