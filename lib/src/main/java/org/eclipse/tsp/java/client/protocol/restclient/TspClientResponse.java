17ee9a2e073abbfdeea3643cf6dfc2cb040aec36
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-17 20:24:02 +0000

    remove text attribute

--- lib/src/main/java/org/eclipse/tsp/java/client/protocol/restclient/TspClientResponse.java
+++ lib/src/main/java/org/eclipse/tsp/java/client/protocol/restclient/TspClientResponse.java
@@ -3,26 +3,19 @@ package org.eclipse.tsp.java.client.protocol.restclient;
 import jakarta.ws.rs.core.Response.Status;
 
 public class TspClientResponse<T> {
-    private String text;
     private Status statusCode;
     private String statusMessage;
     private T responseModel;
 
-    public TspClientResponse(String text, Status statusCode, String statusMessage, T responseModel) {
-        this.text = text;
+    public TspClientResponse(Status statusCode, String statusMessage, T responseModel) {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
         this.responseModel = responseModel;
     }
 
-    public TspClientResponse(String text, Status statusCode, String statusMessage) {
-        this.text = text;
+    public TspClientResponse(Status statusCode, String statusMessage) {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
-    }
-
-    public String getText() {
-        return text;
     }
 
     public Status getStatusCode() {
