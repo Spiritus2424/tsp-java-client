56928ff33c89084101dbff1542380b7f8e50f316
Ahmad Faour <ahmad.faour@polymtl.ca>
2022-08-31 18:59:10 +0000

    add unit test for RestClient class

--- lib/src/test/java/org/eclipse/tsp/java/client/protocol/restclient/RestClientTest.java
+++ lib/src/test/java/org/eclipse/tsp/java/client/protocol/restclient/RestClientTest.java
@@ -1,13 +1,9 @@
 package org.eclipse.tsp.java.client.protocol.restclient;
 
+import org.eclipse.tsp.java.client.models.annotation.Annotation;
 import org.junit.jupiter.api.Test;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
-
-import jakarta.ws.rs.core.MediaType;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
@@ -16,103 +12,45 @@ import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 
+@WireMockTest(httpPort = 8080)
 public class RestClientTest {
-
-    public static WireMockServer wireMockServer = new WireMockServer();
-
-    @BeforeAll
-    static void setupAll() {
-        wireMockServer.start();
-    }
-
-    @AfterAll
-    static void tearDownAll() {
-        wireMockServer.stop();
-    }
-
-    // @DisplayName("Single test successful")
-    // @Test
-    // void testSingleSuccessTest() {
-    // System.out.print("Sucess");
-
-    // }
-
-    // @Test
-    // @Disabled("Not implemented yet")
-    // void testShowSomething() {
-    // }
 
     @Test
     public void getMethod() {
-        stubFor(get("/my/resource")
-                .withHeader("Content-Type", containing("json"))
-                .willReturn(ok()
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBody("{}")));
-
-        TspClientResponse<String> result = RestClient.get("/my/resource",
+        stubFor(get("/my/resource").willReturn(ok()));
+        TspClientResponse<String> result = RestClient.get("http://localhost:8080/my/resource",
                 Optional.empty());
+
         assertTrue(result.isOk());
 
-        // verify(postRequestedFor(urlPathEqualTo("/my/resource"))
-        // .withRequestBody(matching(".*message-1234.*"))
-        // .withHeader("Content-Type", equalTo("text/xml")));
     }
 
     @Test
     public void postMethod() {
-        stubFor(post("/my/resource")
-                .withHeader("Content-Type", containing("xml"))
-                .willReturn(ok()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<response>SUCCESS</response>")));
+        stubFor(post("/my/resource").willReturn(ok()));
+        TspClientResponse<String> result = RestClient.post("http://localhost:8080/my/resource", Optional.empty());
+        assertTrue(result.isOk());
 
-        // TspClientResponse<String> result = RestClient.post("/my/resource",
-        // Optional.empty());
-        // assertTrue(result.isOk());
-
-        // verify(postRequestedFor(urlPathEqualTo("/my/resource"))
-        // .withRequestBody(matching(".*message-1234.*"))
-        // .withHeader("Content-Type", equalTo("text/xml")));
     }
 
     @Test
     public void putMethod() {
-        stubFor(post("/my/resource")
-                .withHeader("Content-Type", containing("xml"))
-                .willReturn(ok()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<response>SUCCESS</response>")));
-
-        // TspClientResponse<String> result = RestClient.post("/my/resource",
-        // Optional.empty());
-        // assertTrue(result.isOk());
-
-        // verify(postRequestedFor(urlPathEqualTo("/my/resource"))
-        // .withRequestBody(matching(".*message-1234.*"))
-        // .withHeader("Content-Type", equalTo("text/xml")));
+        stubFor(put("/my/resource").willReturn(ok()));
+        Annotation annotation = new Annotation("label", BigInteger.ZERO, BigInteger.ZERO, 0, "type");
+        TspClientResponse<Annotation> result = RestClient.put("http://localhost:8080/my/resource", annotation);
+        assertTrue(result.isOk());
     }
 
     @Test
     public void deleteMethod() {
-        stubFor(post("/my/resource")
-                .withHeader("Content-Type", containing("xml"))
-                .willReturn(ok()
-                        .withHeader("Content-Type", "text/xml")
-                        .withBody("<response>SUCCESS</response>")));
-
-        // TspClientResponse<String> result = RestClient.post("/my/resource",
-        // Optional.empty());
-        // assertTrue(result.isOk());
-
-        // verify(postRequestedFor(urlPathEqualTo("/my/resource"))
-        // .withRequestBody(matching(".*message-1234.*"))
-        // .withHeader("Content-Type", equalTo("text/xml")));
+        stubFor(delete("/my/resource").willReturn(ok()));
+        TspClientResponse<String> result = RestClient.delete("http://localhost:8080/my/resource", Optional.empty());
+        assertTrue(result.isOk());
     }
 
 }
