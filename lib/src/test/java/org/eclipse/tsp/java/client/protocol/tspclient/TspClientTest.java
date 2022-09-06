cbaadd20a54d5c831c93cba9c464036cb9e5755d
Ahmad Faour <98.ahmad.faour@gmail.com>
2022-09-06 20:24:29 +0000

    add unit test: create and delete experiment

--- lib/src/test/java/org/eclipse/tsp/java/client/protocol/tspclient/TspClientTest.java
+++ lib/src/test/java/org/eclipse/tsp/java/client/protocol/tspclient/TspClientTest.java
@@ -1,13 +1,21 @@
 package org.eclipse.tsp.java.client.protocol.tspclient;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.tsp.java.client.models.experiment.Experiment;
 import org.eclipse.tsp.java.client.models.health.Health;
 import org.eclipse.tsp.java.client.models.health.HealthStatus;
+import org.eclipse.tsp.java.client.models.query.Query;
 import org.eclipse.tsp.java.client.protocol.restclient.TspClientResponse;
 import org.junit.jupiter.api.Test;
 
@@ -30,5 +38,36 @@ public class TspClientTest {
         assertTrue(response.isOk());
         assertEquals(HealthStatus.UP, response.getResponseModel().getStatus());
 
+    }
+
+    @Test
+    public void createExperiment() {
+        stubFor(post("/experiments").willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBodyFile(String.format("%s/create-experiment-0.json", FIXTURE_PATH))));
+
+        Map<String, Object> parameters = new HashMap<>();
+
+        Query query = new Query(parameters);
+        TspClientResponse<Experiment> response = tspClient.createExperiment(query);
+        assertTrue(response.isOk());
+        assertEquals(response.getResponseModel().getName(), "kernel");
+        assertEquals(response.getResponseModel().getUuid(), "22222222-2222-2222-2222-222222222222");
+        assertEquals(response.getResponseModel().getStart(), new BigInteger("1234567890123456789"));
+        assertEquals(response.getResponseModel().getIndexingStatus(), "COMPLETED");
+    }
+
+    @Test
+    public void deleteExperiment() {
+        final String uuid = "1234567890123456789";
+        stubFor(delete("/experiments/".concat(uuid)).willReturn(aResponse()
+                .withHeader("Content-Type", "application/json")
+                .withBodyFile(String.format("%s/delete-experiment-0.json", FIXTURE_PATH))));
+
+        TspClientResponse<Experiment> response = tspClient.deleteExperiment(uuid);
+        assertTrue(response.isOk());
+        assertEquals(response.getResponseModel().getName(), "kernel");
+        assertEquals(response.getResponseModel().getUuid(), "22222222-2222-2222-2222-222222222222");
+        assertEquals(response.getResponseModel().getIndexingStatus(), "CLOSED");
     }
 }
