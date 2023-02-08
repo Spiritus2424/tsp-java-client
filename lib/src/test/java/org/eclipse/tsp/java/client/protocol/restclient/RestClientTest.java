de606fefa96ecfdd67712ac6a9c9f22d755d549e
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-08 20:37:49 +0000

    Add unit test

--- lib/src/test/java/org/eclipse/tsp/java/client/protocol/restclient/RestClientTest.java
+++ lib/src/test/java/org/eclipse/tsp/java/client/protocol/restclient/RestClientTest.java
@@ -2,6 +2,7 @@ package org.eclipse.tsp.java.client.protocol.restclient;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
 import static com.github.tomakehurst.wiremock.client.WireMock.ok;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.put;
@@ -20,10 +21,12 @@ import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 @WireMockTest(httpPort = 8080)
 public class RestClientTest {
 
+    private static final String MOCK_URL = "/my/resource";
+
     @Test
     public void getMethod() {
-        stubFor(get("/my/resource").willReturn(ok()));
-        TspClientResponse<String> result = RestClient.get("http://localhost:8080/my/resource",
+        stubFor(get(MOCK_URL).willReturn(ok()));
+        TspClientResponse<String> result = RestClient.get("http://localhost:8080".concat(MOCK_URL),
                 Optional.empty(), String.class);
 
         assertTrue(result.isOk());
@@ -31,26 +34,36 @@ public class RestClientTest {
     }
 
     @Test
-    public void postMethod() {
-        stubFor(post("/my/resource").willReturn(ok()));
-        TspClientResponse<String> result = RestClient.post("http://localhost:8080/my/resource", Optional.empty(),
+    public void postMethodOk() {
+        stubFor(post(MOCK_URL).willReturn(ok()));
+        TspClientResponse<String> result = RestClient.post("http://localhost:8080".concat(MOCK_URL), Optional.empty(),
                 String.class);
         assertTrue(result.isOk());
 
     }
 
     @Test
+    public void postMethodNotFound() {
+        stubFor(post(MOCK_URL).willReturn(notFound()));
+        TspClientResponse<String> result = RestClient.post("http://localhost:8080".concat(MOCK_URL), Optional.empty(),
+                String.class);
+        assertTrue(!result.isOk());
+        assertTrue(result.getStatusCode().getStatusCode() == 404);
+    }
+
+    @Test
     public void putMethod() {
-        stubFor(put("/my/resource").willReturn(ok()));
+        stubFor(put(MOCK_URL).willReturn(ok()));
         Annotation annotation = new Annotation("label", BigInteger.ZERO, BigInteger.ZERO, 0, AnnotationType.CHART);
-        TspClientResponse<Annotation> result = RestClient.put("http://localhost:8080/my/resource", annotation);
+        TspClientResponse<Annotation> result = RestClient.put("http://localhost:8080".concat(MOCK_URL), annotation,
+                Annotation.class);
         assertTrue(result.isOk());
     }
 
     @Test
     public void deleteMethod() {
-        stubFor(delete("/my/resource").willReturn(ok()));
-        TspClientResponse<String> result = RestClient.delete("http://localhost:8080/my/resource", Optional.empty(),
+        stubFor(delete(MOCK_URL).willReturn(ok()));
+        TspClientResponse<String> result = RestClient.delete("http://localhost:8080".concat(MOCK_URL), Optional.empty(),
                 String.class);
         assertTrue(result.isOk());
     }
