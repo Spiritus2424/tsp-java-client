0f77f251d98a908a488a25ff39e28fc338abdb91
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-20 22:34:40 +0000

    use lombok to MarkerSet model package

--- lib/src/main/java/org/eclipse/tsp/java/client/models/markerset/MarkerSet.java
+++ lib/src/main/java/org/eclipse/tsp/java/client/models/markerset/MarkerSet.java
@@ -1,46 +1,15 @@
 package org.eclipse.tsp.java.client.models.markerset;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
 public class MarkerSet {
+    @NonNull
     private String name;
+    @NonNull
     private String id;
-
-    public MarkerSet() {
-    }
-
-    public MarkerSet(String name, String id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        boolean isEquals = false;
-        if (this == object)
-            isEquals = true;
-        else if (object == null || getClass() != object.getClass())
-            isEquals = false;
-        else {
-            MarkerSet markerSet = (MarkerSet) object;
-            isEquals = this.id.equals(markerSet.id);
-        }
-
-        return isEquals;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
