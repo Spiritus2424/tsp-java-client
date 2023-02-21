dfac7546fd210669fc42baa91a24c3fe414877c6
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-21 02:24:24 +0000

    use lombok to Style model package

--- lib/src/main/java/org/eclipse/tsp/java/client/models/style/OutputElementStyle.java
+++ lib/src/main/java/org/eclipse/tsp/java/client/models/style/OutputElementStyle.java
@@ -1,34 +1,16 @@
 package org.eclipse.tsp.java.client.models.style;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
 public class OutputElementStyle {
+    @NonNull
     private String parentKey;
+    @NonNull
     private Map<String, Object> values;
-
-    public OutputElementStyle() {
-        this.values = new HashMap<>();
-    }
-
-    public OutputElementStyle(String parentKey, Map<String, Object> values) {
-        this.parentKey = parentKey;
-        this.values = values;
-    }
-
-    public String getParentKey() {
-        return parentKey;
-    }
-
-    public void setParentKey(String parentKey) {
-        this.parentKey = parentKey;
-    }
-
-    public Map<String, Object> getValues() {
-        return values;
-    }
-
-    public void setValues(Map<String, Object> values) {
-        this.values = values;
-    }
 }
--- lib/src/main/java/org/eclipse/tsp/java/client/models/style/OutputStyleModel.java
+++ lib/src/main/java/org/eclipse/tsp/java/client/models/style/OutputStyleModel.java
@@ -1,24 +1,15 @@
 package org.eclipse.tsp.java.client.models.style;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
 public class OutputStyleModel {
+    @NonNull
     private Map<String, OutputElementStyle> styles;
 
-    public OutputStyleModel() {
-        this.styles = new HashMap<>();
-    }
-
-    public OutputStyleModel(Map<String, OutputElementStyle> styles) {
-        this.styles = styles;
-    }
-
-    public Map<String, OutputElementStyle> getStyles() {
-        return styles;
-    }
-
-    public void setStyles(Map<String, OutputElementStyle> styles) {
-        this.styles = styles;
-    }
 }
