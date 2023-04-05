2daa164ce839b25b82c08d7b9471c812740de322
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-05 19:50:25 +0000

    change space to tab

--- lib/src/main/java/org/eclipse/tsp/java/client/api/annotation/AnnotationApi.java
+++ lib/src/main/java/org/eclipse/tsp/java/client/api/annotation/AnnotationApi.java
@@ -14,39 +14,39 @@ import org.eclipse.tsp.java.client.shared.response.GenericResponse;
 import com.fasterxml.jackson.core.type.TypeReference;
 
 public class AnnotationApi extends AbstractTspApi {
-    private final String ANNOTATION_API_URL = "%s/experiments/%s/outputs/%s/annotations";
+	private final String ANNOTATION_API_URL = "%s/experiments/%s/outputs/%s/annotations";
 
-    public AnnotationApi(String baseUrl) {
-        super(baseUrl);
-    }
+	public AnnotationApi(String baseUrl) {
+		super(baseUrl);
+	}
 
-    public TspClientResponse<GenericResponse<AnnotationCategoriesModel>> getAnnotationsCategories(
-            UUID experimentUuid, String outputId, Optional<String> markerSetId) {
-        Map<String, String> queryParameters = null;
-        if (markerSetId.isPresent()) {
-            queryParameters = new HashMap<String, String>();
-            queryParameters.put("markserId", markerSetId.get());
-        }
+	public TspClientResponse<GenericResponse<AnnotationCategoriesModel>> getAnnotationsCategories(
+			UUID experimentUuid, String outputId, Optional<String> markerSetId) {
+		Map<String, String> queryParameters = null;
+		if (markerSetId.isPresent()) {
+			queryParameters = new HashMap<String, String>();
+			queryParameters.put("markserId", markerSetId.get());
+		}
 
-        final TspClientResponse<String> tspClientResponse = RestClient.get(
-                String.format(this.ANNOTATION_API_URL, this.getBaseUrl(), experimentUuid, outputId),
-                markerSetId.isPresent() ? Optional.of(queryParameters) : Optional.empty(), String.class);
+		final TspClientResponse<String> tspClientResponse = RestClient.get(
+				String.format(this.ANNOTATION_API_URL, this.getBaseUrl(), experimentUuid, outputId),
+				markerSetId.isPresent() ? Optional.of(queryParameters) : Optional.empty(), String.class);
 
-        return TspClientResponse.getGenericResponse(tspClientResponse,
-                new TypeReference<GenericResponse<AnnotationCategoriesModel>>() {
-                });
-    }
+		return TspClientResponse.getGenericResponse(tspClientResponse,
+				new TypeReference<GenericResponse<AnnotationCategoriesModel>>() {
+				});
+	}
 
-    public TspClientResponse<GenericResponse<AnnotationModel>> getAnnotations(UUID experimentUuid, String outputId,
-            Query query) {
+	public TspClientResponse<GenericResponse<AnnotationModel>> getAnnotations(UUID experimentUuid,
+			String outputId,
+			Query query) {
+		final TspClientResponse<String> tspClientResponse = RestClient.post(
+				String.format(this.ANNOTATION_API_URL, this.getBaseUrl(), experimentUuid, outputId),
+				Optional.of(query), String.class);
 
-        final TspClientResponse<String> tspClientResponse = RestClient.post(
-                String.format(this.ANNOTATION_API_URL, this.getBaseUrl(), experimentUuid, outputId),
-                Optional.of(query), String.class);
-
-        return TspClientResponse.getGenericResponse(tspClientResponse,
-                new TypeReference<GenericResponse<AnnotationModel>>() {
-                });
-    }
+		return TspClientResponse.getGenericResponse(tspClientResponse,
+				new TypeReference<GenericResponse<AnnotationModel>>() {
+				});
+	}
 
 }
