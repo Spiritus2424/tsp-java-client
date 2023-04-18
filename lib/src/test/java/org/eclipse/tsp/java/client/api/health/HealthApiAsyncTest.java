c8e7caff461de4e5eb0720ad0eedcabf3c7e1d1c
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-18 07:16:02 +0000

    add jackson provider

---
+++ lib/src/test/java/org/eclipse/tsp/java/client/api/health/HealthApiAsyncTest.java
@@ -0,0 +1,32 @@
+package org.eclipse.tsp.java.client.api.health;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+
+@WireMockTest(httpPort = 8080)
+public class HealthApiAsyncTest {
+	private static final String FIXTURE_PATH = "fixtures/tspclient";
+	private static final String TSP_EXTENSION_URL = "/tsp/api";
+
+	private HealthApiAsync healthApiAsync = new HealthApiAsync("http://localhost:8080");
+
+	@Test
+	public void fetchCheckHealth() {
+		final String targetUrl = String.format("%s/health", TSP_EXTENSION_URL);
+		stubFor(get(targetUrl).willReturn(aResponse()
+				.withStatus(200)
+				.withHeader("Content-Type", "application/json")
+				.withBodyFile(String.format("%s/fetch-check-health-0.json", FIXTURE_PATH))));
+
+		Health response = this.healthApiAsync.checkHealth()
+				.thenApply(result -> result.getResponseModel()).join();
+
+		assertEquals(response.getStatus(), HealthStatus.UP);
+	}
+}
