ecf6f713fa8cb5de331d47fbb91731d4366a2a9f
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-24 16:46:27 +0000

    update jersey dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
 
 	// Custom Annotation
 	annotationProcessor(project(":annotationprocessor"))
-	compileOnly(project(":annotationprocessor"))
+	implementation(project(":annotationprocessor"))
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
@@ -49,9 +49,9 @@ dependencies {
     
     // Jarkarta - HTTP implementation
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
-    implementation("org.glassfish.jersey.core:jersey-client:3.1.0-M3")
-    implementation("org.glassfish.jersey.inject:jersey-hk2:3.1.0-M3")
-    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:3.1.0-M3")  
+    implementation("org.glassfish.jersey.core:jersey-client:3.1.1")
+    implementation("org.glassfish.jersey.inject:jersey-hk2:3.1.1")
+    implementation("org.glassfish.jersey.media:jersey-media-json-jackson:3.1.1")  
 
     // Lombok - Decorator
     compileOnly("org.projectlombok:lombok:1.18.26")
---
+++ lib/src/test/java/org/eclipse/tsp/java/client/api/trace/TraceApiAsyncTest.java
@@ -0,0 +1,45 @@
+package org.eclipse.tsp.java.client.api.trace;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+
+import org.eclipse.tsp.java.client.core.tspclient.TspClientResponse;
+import org.eclipse.tsp.java.client.shared.indexing.IndexingStatus;
+import org.eclipse.tsp.java.client.shared.query.Query;
+import org.junit.jupiter.api.Test;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+
+@WireMockTest(httpPort = 8080)
+public class TraceApiAsyncTest {
+
+	private static final String FIXTURE_PATH = "fixtures/tspclient";
+	private static final String TSP_EXTENSION_URL = "/tsp/api";
+
+	private TraceApiAsync traceApiAsync = new TraceApiAsync("http://localhost:8080",
+			Executors.newSingleThreadExecutor());
+
+	@Test
+	public void openTrace() {
+		final String targetUrl = String.format("%s/traces", TSP_EXTENSION_URL);
+		stubFor(post(targetUrl).willReturn(aResponse()
+				.withHeader("Content-Type", "application/json")
+				.withBodyFile(String.format("%s/open-trace-0.json", FIXTURE_PATH))));
+
+		Map<String, Object> parameters = new HashMap<>();
+		Query query = new Query(parameters);
+		TspClientResponse<Trace> response = this.traceApiAsync.openTrace(query).join();
+
+		assertEquals(IndexingStatus.CLOSED, response.getResponseModel().getIndexingStatus());
+		assertEquals("kernel", response.getResponseModel().getName());
+		assertEquals(UUID.fromString("11111111-1111-1111-1111-111111111111"), response.getResponseModel().getUuid());
+	}
+
+}
