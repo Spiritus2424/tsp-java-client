82bf0bd7b3ed1fccc5d6b41b690f4ad5cbb93838
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-27 05:19:01 +0000

    Add ObjectMapperContextResolver

---
+++ lib/src/main/java/org/eclipse/tsp/java/client/core/objectmapper/ObjectMapperContextResolver.java
@@ -0,0 +1,30 @@
+package org.eclipse.tsp.java.client.core.objectmapper;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+import jakarta.ws.rs.ext.ContextResolver;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+
+	private final ObjectMapper objectMapper;
+
+	public ObjectMapperContextResolver() {
+		this.objectMapper = JsonMapper.builder().enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS).build();
+
+		this.objectMapper.configure(DeserializationFeature.READ_ENUMS_USING_TO_STRING, true)
+				.configure(SerializationFeature.WRITE_ENUMS_USING_TO_STRING, true);
+
+	}
+
+	@Override
+	public ObjectMapper getContext(Class<?> type) {
+		return this.objectMapper;
+	}
+
+}
